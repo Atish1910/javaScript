7cc0c2c11c76f60c35ac5864966dba19d4fc670f
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2025-02-25 19:16:52 +0530

    dfdf

--- js/patterns/70.js
+++ js/patterns/70.js
@@ -1,23 +1,29 @@
-/*
-    Input : 
 
-    Row : 6
-    Col : 6
+    // Input : 
 
-    Output : 
-    $   $   $   $   $   $
-    $   *               $
-    $       *           $
-    $           *       $
-    $               *   $
-    $   $   $   $   $   $
+    // Row : 6
+    // Col : 6
+
+    // Output : 
+    // @ @ @ @ @ @
+    // @ # & & & @
+    // @ & # & & @
+    // @ & & # & @
+    // @ & & & # @
+    // @ @ @ @ @ @
                     
-*/   
+// Hardcoded values
+iRow = 6, iCol = 6;
+
+
+Display(iRow, iCol);
 function Display(iRow, iCol) {
     for(i = 1; i <= iRow; i++){
         for(j = 1; j <= iCol; j++){
-            if(i == 1|| i == iRow || j == 1 || j == iCol || i == j){
+            if(i == 1|| j == 1 || j == iCol || i == iRow){
                 document.write("@ \t");
+            }else if( i == j){
+                document.write("# \t");
             }
             else{
                 document.write("& \t");
@@ -28,6 +34,4 @@ function Display(iRow, iCol) {
     }
 }
 
-// Hardcoded values
-let iRow = 6, iCol = 6;
-Display(iRow, iCol);
+
